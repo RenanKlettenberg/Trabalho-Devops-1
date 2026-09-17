08bec87bcbfeaa237813cb785dd521a8ad3efc22
juliapavanello <julia.pavanello21@gmail.com>
2026-09-16 21:20:38 -0300

    alteração banco service-usuarios para cumprir requisito

--- service-usuario/init.sql
+++ service-usuario/init.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS usuario.usuarios (
 	usu_email VARCHAR(256) UNIQUE NOT NULL,
 	usu_nome VARCHAR(256) NOT NULL,
 	usu_password TEXT NOT NULL,
+	usu_plano VARCHAR(20) NOT NULL DEFAULT 'free',
 	usu_codigo_auth INT,
 	usu_codigo_auth_validade TIMESTAMP,
 	usu_created_at TIMESTAMP DEFAULT NOW(),
