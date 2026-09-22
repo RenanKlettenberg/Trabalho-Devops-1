8368858d063af8f45d64ad4df847d936a209702b
Renan G. Klettenberg <121028916+RenanKlettenberg@users.noreply.github.com>
2026-09-22 07:58:02 -0300

    fix: esqueci de algo

--- service-viagens/src/service/evento.service.js
+++ service-viagens/src/service/evento.service.js
@@ -1,6 +1,6 @@
 import AppError from "../infrastructure/errors/app.error.js";
 import RESPONSE from "../shared/constants/response.js";
-import { STATUS_EVENTO } from "../shared/constants/evento.constants.js";
+import { CATEGORIA_EVENTO, STATUS_EVENTO } from "../shared/constants/evento.constants.js";
 
 function criarServiceEvento(repository, despesaClient) {
     async function listar(filtros = {}) {
@@ -30,7 +30,7 @@ function criarServiceEvento(repository, despesaClient) {
             try {
                 const despesa = await despesaClient.criarDespesa({
                     descricao: evento.eve_descricao,
-                    categoria: evento.eve_categoria,
+                    categoria: CATEGORIA_EVENTO[evento.eve_categoria],
                     valor: evento.eve_orcamento,
                     eventoId: evento.eve_id,
                     viagemId: evento.via_id,
